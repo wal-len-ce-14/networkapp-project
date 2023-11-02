e0a77651b0463b6b9b4fb2b51717be63f9f0bfab
wallence <a33369097@gmail.com>
2023-11-02 22:53:55 +0800

    add post operation
    無法加新的工作在input下面
    要createChile來加入新的html_tag但會被post刷新
    待解決

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// const http = require("http");
 const fs = require("fs")
 const path = require("path");
 const express = require("express")
+const bodyParser = require('body-parser');
 const port = 8080;
 
 
@@ -10,13 +10,25 @@ app = express();
 app.set('views', path.join(__dirname, "webpage"));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res)=>{
     res.render('index.jade',{
-        title: "My Network App project!!!",
-        numoftask: 1
+        title: "My Network App project!!!"
     });
 });
+
+app.post('/', (req, res)=>{
+    task = "Add " + req.body.todo
+    if(task=="Add ") task = "Task cannot be blank"
+
+    res.render('index.jade',{
+        title: "My Network App project!!!",
+        addthing: `${task}`
+    });
+    return req.body.todo
+})
 
 app.listen(port, ()=>{
     console.log(`App is listening on port ${port}...`)
---
+++ public/javascribe/index.js
@@ -0,0 +1,25 @@
+
+const taskContainer = document.getElementById('taskContainer')
+const addtask = document.getElementById("addtask");
+const input = document.getElementById("taskname");
+addtask.addEventListener('click', async (e)=>{
+    e.preventDefault()
+    try{
+        // task = axios.post("/", {
+        //     todo: input.value
+        // })
+        task = input.value.todo
+        console.log(task)
+        const tasktag = document.createElement('h5');
+        if(task != ''){
+            tasktag.textContent = task;
+            taskContainer.appendChild(tasktag);
+        }
+    }catch(e){
+        console.log(e)
+    }
+    
+    
+      
+})
+
