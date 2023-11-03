df6f3c97d97e5d1e96da38030837685da607eceb
wallence <a33369097@gmail.com>
2023-11-03 17:15:52 +0800

    add submit operation
    可以上傳工作到myjson檔
    使用task陣列儲存 方便調用
    加入順序 最新的工作在最前面

--- app.js
+++ app.js
@@ -1,34 +1,20 @@
 const fs = require("fs")
 const path = require("path");
 const express = require("express")
-const bodyParser = require('body-parser');
+const homepage = require("./router/homepage")
 const port = 8080;
 
-
-app = express();
+const app = express();
 // app.use('/', express.static(path.join(__dirname, 'html')));
 app.set('views', path.join(__dirname, "webpage"));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.render('index.jade',{
-        title: "My Network App project!!!"
-    });
-});
+app.use('/', homepage)
 
-app.post('/', (req, res)=>{
-    task = "Add " + req.body.todo
-    if(task=="Add ") task = "Task cannot be blank"
 
-    res.render('index.jade',{
-        title: "My Network App project!!!",
-        addthing: `${task}`
-    });
-    return req.body.todo
-})
 
 app.listen(port, ()=>{
     console.log(`App is listening on port ${port}...`)
---
+++ router/homepage.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const router = express.Router();
+router.use(express.urlencoded({ extended: false }));
+router.use(express.json());
+router.use(express.static(path.join(__dirname, 'public')));
+
+const taskfile = 'mytask/task.json'
+const taskJson = JSON.parse(fs.readFileSync(taskfile, 'utf8'));
+
+
+router.get('/', (req, res)=>{
+    
+    res.render('index.jade',{
+        title: "My Network App project!!!",
+        tasks: taskJson.task
+    });
+});
+
+router.post('/', (req, res)=> {
+    
+
+    let task = "Add " + req.body.todo
+    if(task != "Add ") {
+        taskJson.task.push(req.body.todo)
+        // 
+        console.log(taskJson)
+    }else {
+        task = "Task cannot be blank"
+    }   
+
+    res.status(200).render('index.jade',{
+        title: "My Network App project!!!",
+        addthing: `${task}`,
+        tasks: taskJson.task
+    });
+    
+    fs.writeFileSync(taskfile, JSON.stringify(taskJson, null, 2), 'utf8')
+    
+})
+
+
+module.exports = router
