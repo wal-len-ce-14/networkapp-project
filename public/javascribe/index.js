652b6649c2e2d1c940a6b9a2b9ef465aaa00b4ff
wallence <a33369097@gmail.com>
2023-11-12 00:52:24 +0800

    add describe bar to describe task

--- public/javascribe/index.js
+++ public/javascribe/index.js
@@ -1,5 +1,18 @@
+let input_todo = document.getElementById("taskname")
+let tododescribe = document.getElementById("taskdescribe")
+let input_todo_form = document.querySelector(".todo-form")
+
+input_todo.addEventListener('click', ()=>{
+    tododescribe.style.display = 'block'
+})
+tododescribe.addEventListener('blur', ()=>{
+    tododescribe.style.display = 'none'
+})
+
+
 let tasks = document.querySelectorAll('.task')
 let doneSubmit = document.querySelectorAll('.done-submit')
+
 tasks.forEach((task, idx) => {
     task.style.backgroundColor = `rgb(${10*idx+25},80, ${240-10*idx} )`
     doneSubmit[idx].style.borderColor = `rgb(${10*idx+25},80, ${240-10*idx} )`
@@ -7,5 +20,6 @@ tasks.forEach((task, idx) => {
         window.location.href = `/todo/${task.firstElementChild.textContent}`;
     })
 });
+
 
 
