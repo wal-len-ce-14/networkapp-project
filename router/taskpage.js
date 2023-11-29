dfae4809e675d31b33d82877cbe7802cabcfd1c4
DDK1127 <derek61003@gmail.com>
2023-11-29 13:43:17 +0800

    fix task problem.

--- router/taskpage.js
+++ router/taskpage.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 const router = express.Router();
 router.use(express.urlencoded({ extended: false }));
 router.use(express.json());
-const taskfile = 'mytask/task.json'
-const taskJson = JSON.parse(fs.readFileSync(taskfile, 'utf8'));
+const taskfile = './mytask/task.json'
 
 router.get('/', (req, res)=>{
+    const taskJson = JSON.parse(fs.readFileSync(taskfile, 'utf8'));
     express().set('view engine', 'ejs');
     res.status(200).render('taskpage.ejs',taskJson.task[req.query.taskid])
 })
