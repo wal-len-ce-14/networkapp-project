cf5367d74866ec9b25d0b26ad44c388a61eb6c43
DDK1127 <derek61003@gmail.com>
2023-12-02 15:12:57 +0800

    can't find post path problem.

--- router/taskpage.js
+++ router/taskpage.js
@@ -7,8 +7,17 @@ const taskfile = './mytask/task.json'
 
 router.get('/', (req, res)=>{
     const taskJson = JSON.parse(fs.readFileSync(taskfile, 'utf8'));
+    const taskid = req.query.taskid;
     express().set('view engine', 'ejs');
-    res.status(200).render('taskpage.ejs',taskJson.task[req.query.taskid])
+    res.status(200).render('taskpage.ejs',taskJson.task[taskid]);
+})
+
+router.post('/todo/task/edit', (req, res)=>{
+    console.log("i am here");
+    const taskJson = JSON.parse(fs.readFileSync(taskfile, 'utf8'));
+    const edit_describe = req.body.edit-describe;
+    console.log(edit_describe);
+    express().set('view engine', 'ejs');
 })
 
 module.exports = router
