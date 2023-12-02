f3baa74eb2b384841b3a6c559d51e93305e4b7cc
DDK1127 <derek61003@gmail.com>
2023-12-02 17:17:10 +0800

    finish css & edit function.

--- router/taskpage.js
+++ router/taskpage.js
@@ -4,20 +4,27 @@ const router = express.Router();
 router.use(express.urlencoded({ extended: false }));
 router.use(express.json());
 const taskfile = './mytask/task.json'
+var taskid;
 
 router.get('/', (req, res)=>{
     const taskJson = JSON.parse(fs.readFileSync(taskfile, 'utf8'));
-    const taskid = req.query.taskid;
+    taskid = req.query.taskid;
     express().set('view engine', 'ejs');
     res.status(200).render('taskpage.ejs',taskJson.task[taskid]);
 })
 
-router.post('/todo/task/edit', (req, res)=>{
-    console.log("i am here");
+router.post('/', (req, res)=>{
     const taskJson = JSON.parse(fs.readFileSync(taskfile, 'utf8'));
-    const edit_describe = req.body.edit-describe;
-    console.log(edit_describe);
     express().set('view engine', 'ejs');
+    const editdescribe = req.body.editdescribe;
+
+    // console.log(taskid);
+    taskJson.task[taskid].describe = editdescribe;
+    fs.writeFileSync(taskfile, JSON.stringify(taskJson, null, 2), 'utf8');
+    console.log(taskJson);
+    console.log(editdescribe);
+    res.redirect('/');
+
 })
 
 module.exports = router
